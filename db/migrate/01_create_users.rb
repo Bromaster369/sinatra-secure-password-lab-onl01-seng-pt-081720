039caeac320d215150a41f335dafca3be2f6e31e
Christian Pardo <chrispardopersonal@gmail.com>
2020-12-09 17:12:16 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   
   def change 
-    create_table
+    create_table :users do |t|
+      t.string :username
+      t.string :password 
+    end 
   end 
   
 end 
