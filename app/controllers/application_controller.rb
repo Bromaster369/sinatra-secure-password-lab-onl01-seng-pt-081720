0f0876ce9130a940874a78f56148c38e86af8c7e
Christian Pardo <chrispardopersonal@gmail.com>
2020-12-09 17:21:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     ##your code here
     user = find_by(:username => params[:username])
     
-    if user && authenticate 
+    if user && user.authenticate 
   end
 
   get "/failure" do
